d3bdfcbf5f070a06241891bb52c00c982e058c63
Seokhee-Jin <seokhee749@gmail.com>
2023-05-08 00:47:53 +0900

    final

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from preprocess.face_alignment import image_align
+from ffhq_dataset.gen_aligned_image import FaceAlign
+import cv2
 
 if __name__ == "__main__":
-    image_align()
+    a = FaceAlign()
+    b = cv2.imread("/Users/jinseokhee/Pictures/instagram/343973427_270575548657408_8881576880503537909_n.jpg")
+    c = a.get_crop_image(b)
+    cv2.imwrite("./test.jpg", c)
+
+#
